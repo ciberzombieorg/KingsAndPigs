e7eb1495ba2a85befaaac503561167e762f300bd
Alfredo Ricardo Aguilar Hernandez <alfredo-iker@hotmail.com>
2025-01-23 15:25:59 -0600

    Se agregó el pivote en el centro del personaje para mejor animación de giro

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D m_rigidbody2D;
     private GatherInput m_gatherInput;
     private Transform m_transform;
-    [SerializeField] private float speed;
+    [SerializeField] private float speed = 4;
+    private int direction = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,6 +19,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        Flip();
+
         m_rigidbody2D.linearVelocity = new Vector2(speed * m_gatherInput.ValueX, m_rigidbody2D.linearVelocityY);
+
+    }
+
+    private void Flip()
+    {
+        if (m_gatherInput.ValueX * direction < 0)
+        {
+            m_transform.localScale = new Vector3(-m_transform.localScale.x, 1, 1);
+            direction *= -1;
+        }
     }
 }
